13bbe3fa24b89a9d71d52d726349271cd95af5be
Amarildo Miguel <amarildomgl@gmail.com>
2023-05-12 14:19:55 +0100

    testar classe Pessoa

--- src/main/java/org/ucan/Main.java
+++ src/main/java/org/ucan/Main.java
@@ -1,7 +1,12 @@
 package org.ucan;
 
+import org.ucan.entidades.Pessoa;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Pessoa amarildo = new Pessoa("Amarildo Miguel", LocalDate.of(1999, 2, 24));
+        System.out.println(amarildo);
     }
 }
